af612a3f51746f67b6cdf31292d9acce7cc8edac
Anastasia Efremova <anastasia.stoloto@gmail.com>
2025-09-07 18:56:51 +0300

    homework lesson 11.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -11,5 +13,10 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager"; // Дожидаемся полной загрузки страницы
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void tearDown() {
+        Selenide.closeWebDriver();
     }
 }
